ced72338a94d9d2c72fc99dc09f76d535a4a27d3
Yael Keemink <YaelK@CURSISTZ01>
2017-01-10 14:35:40 +0100

    added klantCreated event

---
+++ CAN.Common/src/CAN.Common/Events/KlantCreatedEvent.cs
@@ -0,0 +1,23 @@
+﻿using InfoSupport.WSA.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CAN.Common.Events
+{
+    public class KlantCreatedEvent
+        :DomainEvent
+    {
+        public KlantCreatedEvent(string routingKey) : base(routingKey)
+        {
+        }
+
+        public long Klantnummer { get; set; }
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public string Tussenvoegsels { get; set; }
+        public string Postcode { get; set; }
+        public int Telefoonnummer { get; set; }
+    }
+}
