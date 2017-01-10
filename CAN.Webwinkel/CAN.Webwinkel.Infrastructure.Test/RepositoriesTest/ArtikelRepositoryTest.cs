501964e795e2cf0a80e9f1ce01c2e64a93076a49
Rob Gerritsen <RobG@CURSISTZ03>
2017-01-10 15:07:04 +0100

    Database entities

---
+++ CAN.Webwinkel/CAN.Webwinkel.Infrastructure.Test/RepositoriesTest/ArtikelRepositoryTest.cs
@@ -0,0 +1,65 @@
+﻿using CAN.Webwinkel.Infrastructure.DAL;
+using CAN.Webwinkel.Infrastructure.DAL.Repositories;
+using CAN.Webwinkel.Infrastructure.Test.Provider;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CAN.Webwinkel.Infrastructure.Test.RepositoriesTest
+{
+    [TestClass]
+    public class ArtikelRepositoryTest
+    {
+        private DbContextOptions _options;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestInitialize]
+        public void Init()
+        {
+            // Use InMemory database for testing, records are not removed afterwards from Local Database
+            //_options = TestDatabaseProvider.CreateInMemoryDatabaseOptions();
+            _options = TestDatabaseProvider.CreateMsSQLDatabaseOptions();
+        }
+
+        [TestMethod]
+        public void SaveArtikel()
+        {
+            var demo = new DemoEntities();
+
+            using (var context = new WinkelDatabaseContext(_options))
+            using (var repo = new ArtikelRepository(context))
+            {
+                repo.Insert(demo.HerenFiets);
+                repo.Insert(demo.Fiets);
+                repo.Insert(demo.DamesFiets);
+
+            }
+
+
+            using (var context = new WinkelDatabaseContext(_options))
+            using (var repo = new ArtikelRepository(context))
+            {
+                Assert.AreEqual(3, repo.Count());
+
+                var fiets = repo.Find(demo.Fiets.Artikelnummer);
+                var herenFiets = repo.Find(demo.HerenFiets.Artikelnummer);
+
+                Assert.AreEqual(fiets.ArtikelCategory[0].CategoryId, herenFiets.ArtikelCategory[0].CategoryId);
+            }
+            
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ CAN.Webwinkel/src/CAN.Webwinkel.Infrastructure/DAL/Entities/Artikel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CAN.Webwinkel.Infrastructure.DAL.Entities
+{
+    public class Artikel
+    {
+        public long Id { get; set; }
+        public int Artikelnummer { get; set; }
+        public string Naam { get; set; }
+        public string Beschrijving { get; set; }
+        public decimal Prijs { get; set; }
+
+        public string AfbeeldingUrl { get; set; }
+        public DateTime LeverbaarVanaf { get; set; }
+        public DateTime? LeverbaarTot { get; set; }
+        public string LeverancierCode { get; set; }
+        public string Leverancier { get; set; }
+
+        public List<ArtikelCategory> ArtikelCategory { get; set; } = new List<ArtikelCategory>();
+
+
+
+    }
+}
