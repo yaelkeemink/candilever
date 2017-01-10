e6052fd66c23982d9aa0b7531a1113aa214a52dd
Yael Keemink <YaelK@CURSISTZ01>
2017-01-10 11:19:51 +0100

    changed tests

--- CAN.Klantbeheer/CAN.Klantbeheer/src/CAN.Klantbeheer.Domain/Entities/Klant.cs
+++ CAN.Klantbeheer/CAN.Klantbeheer/src/CAN.Klantbeheer.Domain/Entities/Klant.cs
@@ -1,15 +1,19 @@
-﻿namespace CAN.Klantbeheer.Domain.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CAN.Klantbeheer.Domain.Domain.Entities
 {
     public class Klant
     {
+        [Key]
         public long Klantnummer { get; set; }
-
+        [Required]
         public string Voornaam { get; set; }
-
+        [Required]
         public string Achternaam { get; set; }
 
         public string Tussenvoegsels { get; set; }
 
         public string Postcode { get; set; }
+        public int Telefoonnummer { get; set; }
     }
 }
