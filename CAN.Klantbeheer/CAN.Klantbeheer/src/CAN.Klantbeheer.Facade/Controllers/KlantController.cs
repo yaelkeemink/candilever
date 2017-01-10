4c98a5a92293ae0df3346a5291b148e85306631e
Lars Celie <LarsC@CURSISTZ02>
2017-01-10 14:08:46 +0100

    Added default logging

--- CAN.Klantbeheer/CAN.Klantbeheer/src/CAN.Klantbeheer.Facade/Controllers/KlantController.cs
+++ CAN.Klantbeheer/CAN.Klantbeheer/src/CAN.Klantbeheer.Facade/Controllers/KlantController.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using CAN.Klantbeheer.Domain.Domain.Entities;
 using CAN.Klantbeheer.Facade.Facade.Errors;
 using CAN.Klantbeheer.Domain.Domain.Services;
+using Microsoft.Extensions.Logging;
 
 namespace CAN.Klantbeheer.Facade.Facade.Controllers
 {
@@ -13,35 +14,40 @@ namespace CAN.Klantbeheer.Facade.Facade.Controllers
     public class KlantController : Controller
     {
         private readonly KlantService _service;
+        private readonly ILogger<KlantController> _logger;
 
-        public KlantController(KlantService service)
+        public KlantController(ILogger<KlantController> logger, KlantService service)
         {
+            _logger = logger;
             _service = service;
         }
 
         // POST api/values
-        [HttpPost]        
+        [HttpPost]
         [SwaggerOperation("Post")]
         [ProducesResponseType(typeof(Klant), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(ErrorMessage), (int)HttpStatusCode.BadRequest)]
-        public IActionResult Createklant([FromBody]Klant klant)
+        public IActionResult CreateKlant([FromBody]Klant klant)
         {
+            _logger.LogInformation("Create Klant called", klant);
             if (!ModelState.IsValid)
             {
                 var error = new ErrorMessage(ErrorTypes.BadRequest, "Modelstate Invalide");
+                _logger.LogError("Create Klant Model State invalid", error);
                 return BadRequest(error);
             }
-                try
-                {
-                    var room = _service.CreatePlayer(klant);
-                    return Ok(room);
-                }
-                catch (Exception ex)
-                {
-                    var error = new ErrorMessage(ErrorTypes.Unknown,
-                        $"Onbekende fout in create player: {klant},/nException: {ex}");
-                    return BadRequest(error);
-                }
+            try
+            {
+                var room = _service.CreatePlayer(klant);
+                return Ok(room);
+            }
+            catch (Exception ex)
+            {
+                var error = new ErrorMessage(ErrorTypes.Unknown,
+                    $"Onbekende fout in create player: {klant},/nException: {ex}");
+                _logger.LogError("Create Klant unkown error occured", error);
+                return BadRequest(error);
+            }
 
 
         }
@@ -53,9 +59,11 @@ namespace CAN.Klantbeheer.Facade.Facade.Controllers
         [ProducesResponseType(typeof(ErrorMessage), (int)HttpStatusCode.BadRequest)]
         public IActionResult UpdateKlant([FromBody]Klant klant)
         {
-            if (ModelState.IsValid)
+            _logger.LogInformation("Update Klant called", klant);
+            if (!ModelState.IsValid)
             {
                 var error = new ErrorMessage(ErrorTypes.BadRequest, "Modelstate Invalide");
+                _logger.LogError("Update Klant Model State invalid", error);
                 return BadRequest(error);
             }
             try
@@ -67,12 +75,14 @@ namespace CAN.Klantbeheer.Facade.Facade.Controllers
             {
                 var error = new ErrorMessage(ErrorTypes.NotFound,
                         $"Fout met updaten in db: {klant}/nException: {ex}");
+                _logger.LogError("Update klant failed, klant not found", error);
                 return NotFound(error);
             }
             catch (Exception ex)
             {
                 var error = new ErrorMessage(ErrorTypes.Unknown,
                         $"Onbekende fout bij updaten: {klant}/nException: {ex}");
+                _logger.LogError("Create Klant unkown error occured", error);
                 return BadRequest(error);
             }
         }
