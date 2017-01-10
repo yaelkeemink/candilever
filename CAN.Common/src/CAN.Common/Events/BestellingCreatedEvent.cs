6d4ced17b484de7451cd208492f25a1d33365456
Vincent van de Meent <VincentM@CURSISTZ04>
2017-01-10 15:01:44 +0100

    Added serveral events

---
+++ CAN.Common/src/CAN.Common/Events/BestellingCreatedEvent.cs
@@ -0,0 +1,20 @@
+﻿using InfoSupport.WSA.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CAN.Common.Events
+{
+    public class BestellingCreatedEvent : DomainEvent
+    {
+        public BestellingCreatedEvent(string routingKey) : base(routingKey)
+        {
+        }
+
+        public long Bestellingsnummer { get; set; }
+
+        public DateTime BestelDatum { get; set; }
+
+    }
+}
