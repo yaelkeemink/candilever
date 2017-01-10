9fb1b343f64c8e5bc49fd4c2825b46afd771f992
Vincent van de Meent <VincentM@CURSISTZ04>
2017-01-10 10:42:05 +0100

    Added artikel entity

---
+++ CAN.Bestellingbeheer/CAN.Bestellingbeheer/src/CAN.Bestellingbeheer.Domain/Entities/Artikel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CAN.Bestellingbeheer.Domain.Entities
+{
+    public class Artikel
+    {
+        public long Id { get; set; }
+
+        public decimal Prijs { get; set; }
+    }
+}
--- CAN.Bestellingbeheer/CAN.Bestellingbeheer/src/CAN.Bestellingbeheer.Domain/Entities/Bestelling.cs
+++ CAN.Bestellingbeheer/CAN.Bestellingbeheer/src/CAN.Bestellingbeheer.Domain/Entities/Bestelling.cs
@@ -1,9 +1,12 @@
-﻿namespace CAN.Bestellingbeheer.Domain.Domain.Entities
+﻿using CAN.Bestellingbeheer.Domain.Entities;
+using System.Collections.Generic;
+
+namespace CAN.Bestellingbeheer.Domain.Domain.Entities
 {
     public class Bestelling
     {
         public long Id { get; set; }
 
-        public string Name { get; set; }
+        public IList<Artikel> Artikelen { get; set; }
     }
 }
