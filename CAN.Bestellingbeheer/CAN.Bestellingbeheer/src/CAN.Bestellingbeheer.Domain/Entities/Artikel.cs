9f0f3893834739bb7b54fe8ab1b4dd12e8390193
Vincent van de Meent <VincentM@CURSISTZ04>
2017-01-10 11:53:22 +0100

    Er mist een FK naar Bestelling FIXED

--- CAN.Bestellingbeheer/CAN.Bestellingbeheer/src/CAN.Bestellingbeheer.Domain/Entities/Artikel.cs
+++ CAN.Bestellingbeheer/CAN.Bestellingbeheer/src/CAN.Bestellingbeheer.Domain/Entities/Artikel.cs
@@ -8,6 +8,11 @@ namespace CAN.Bestellingbeheer.Domain.Entities
 {
     public class Artikel
     {
+        public long BestellingId { get; set; }
+
+        public Bestelling Bestelling { get; set; }
+
+
         public long Id { get; set; }
 
         public string Naam { get; set; }
