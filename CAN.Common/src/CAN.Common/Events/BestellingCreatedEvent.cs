d6d414a2658f04d44b786dd2abf90d49a7d3084b
Vincent van de Meent <VincentM@CURSISTZ04>
2017-01-11 09:57:45 +0100

    BestellingCreatedEvent now have Artikelen list instead of ArtikelenCreatedEvent

--- CAN.Common/src/CAN.Common/Events/BestellingCreatedEvent.cs
+++ CAN.Common/src/CAN.Common/Events/BestellingCreatedEvent.cs
@@ -10,11 +10,37 @@ namespace CAN.Common.Events
     {
         public BestellingCreatedEvent(string routingKey) : base(routingKey)
         {
+            Artikelen = new List<Artikel>();
         }
 
         public long Bestellingsnummer { get; set; }
 
         public DateTime BestelDatum { get; set; }
 
+        public IList<Artikel> Artikelen { get; internal set; }
+
+        public void AddArtikel(long artikelNummer, string artikelNaam, decimal prijs, int aantal)
+        {
+            Artikelen.Add(new Artikel(artikelNummer, artikelNaam, prijs, aantal));
+        }
+
+        public class Artikel
+        {
+            internal Artikel(long artikelNummer, string artikelNaam, decimal prijs, int aantal)
+            {
+                Artikelnummer = artikelNummer;
+                Artikelnaam = artikelNaam;
+                Prijs = prijs;
+                Aantal = aantal;
+            }
+
+            public long Artikelnummer { get; set; }
+
+            public string Artikelnaam { get; set; }
+
+            public decimal Prijs { get; set; }
+
+            public int Aantal { get; set; }
+        }
     }
 }
