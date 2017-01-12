82bad9627b37067743539da241d467544cea3c37
Vincent van de Meent <VincentM@CURSISTZ04>
2017-01-12 09:15:05 +0100

    added required klantnummer

--- CAN.Bestellingbeheer/CAN.Bestellingbeheer/src/CAN.Bestellingbeheer.Domain/Entities/Bestelling.cs
+++ CAN.Bestellingbeheer/CAN.Bestellingbeheer/src/CAN.Bestellingbeheer.Domain/Entities/Bestelling.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CAN.Bestellingbeheer.Domain.Entities
 {
@@ -7,6 +8,7 @@ namespace CAN.Bestellingbeheer.Domain.Entities
     {
         public long Bestellingnummer { get; set; }
 
+        [Required]
         public long Klantnummer { get; set; }
 
         public IList<Artikel> Artikelen { get; set; }
