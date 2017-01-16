08598f2a810e3015addffea6f9bd3b695e79cc5c
Rob Gerritsen <RobG@CURSISTZ03>
2017-01-16 13:30:23 +0100

    Fix unit test

--- CAN.Webwinkel/CAN.Webwinkel.Infrastructure.Test/EventListenerTest/ArtikelDispatcherTest.cs
+++ CAN.Webwinkel/CAN.Webwinkel.Infrastructure.Test/EventListenerTest/ArtikelDispatcherTest.cs
@@ -33,33 +33,35 @@ namespace CAN.Webwinkel.Infrastructure.Test.EventListenerTest
         {
             // No strict because we dont need it for this object
             var loggerMock = new Mock<ILogger>();
-            var dispatcher = new ArtikelEventDispatcher(null, _options, loggerMock.Object);
-            var artikelEvent = new EventProvider().FietsCreatedEvent;
-
-
-            dispatcher.ArtikelAanCatalogusToegevoegd(artikelEvent);
-
-            using (var context = new WinkelDatabaseContext(_options))
-            using(var artikelRepo = new ArtikelRepository(context))
+            using (var dispatcher = new ArtikelEventDispatcher(null, _options, loggerMock.Object))
             {
+                var artikelEvent = new EventProvider().FietsCreatedEvent;
 
-                Assert.AreEqual(1, artikelRepo.Count());
 
-                var fiets = artikelRepo.Find(artikelEvent.Artikelnummer);
-                Assert.AreEqual(artikelEvent.Artikelnummer, fiets.Artikelnummer);
-                Assert.AreEqual(artikelEvent.AfbeeldingUrl, fiets.AfbeeldingUrl);
-                Assert.AreEqual(artikelEvent.Beschrijving, fiets.Beschrijving);
-                Assert.AreEqual(artikelEvent.LeverbaarTot, fiets.LeverbaarTot);
-                Assert.AreEqual(artikelEvent.Leverancier, fiets.Leverancier);
-                Assert.AreEqual(artikelEvent.LeverbaarVanaf, fiets.LeverbaarVanaf);
-                Assert.AreEqual(artikelEvent.LeverancierCode, fiets.LeverancierCode);
-                Assert.AreEqual(artikelEvent.Prijs, fiets.Prijs);
-                Assert.AreEqual(artikelEvent.Naam, fiets.Naam);
-                Assert.AreEqual(0, fiets.Voorraad);
+                dispatcher.ArtikelAanCatalogusToegevoegd(artikelEvent);
 
-                Assert.IsNotNull(fiets.ArtikelCategorie[0].Categorie);
-                Assert.IsNotNull(fiets.ArtikelCategorie[0].Categorie.Naam);
-                Assert.AreEqual(artikelEvent.Categorieen[0], fiets.ArtikelCategorie[0].Categorie.Naam);
+                using (var context = new WinkelDatabaseContext(_options))
+                using (var artikelRepo = new ArtikelRepository(context))
+                {
+
+                    Assert.AreEqual(1, artikelRepo.Count());
+
+                    var fiets = artikelRepo.Find(artikelEvent.Artikelnummer);
+                    Assert.AreEqual(artikelEvent.Artikelnummer, fiets.Artikelnummer);
+                    Assert.AreEqual(artikelEvent.AfbeeldingUrl, fiets.AfbeeldingUrl);
+                    Assert.AreEqual(artikelEvent.Beschrijving, fiets.Beschrijving);
+                    Assert.AreEqual(artikelEvent.LeverbaarTot, fiets.LeverbaarTot);
+                    Assert.AreEqual(artikelEvent.Leverancier, fiets.Leverancier);
+                    Assert.AreEqual(artikelEvent.LeverbaarVanaf, fiets.LeverbaarVanaf);
+                    Assert.AreEqual(artikelEvent.LeverancierCode, fiets.LeverancierCode);
+                    Assert.AreEqual(artikelEvent.Prijs, fiets.Prijs);
+                    Assert.AreEqual(artikelEvent.Naam, fiets.Naam);
+                    Assert.AreEqual(0, fiets.Voorraad);
+
+                    Assert.IsNotNull(fiets.ArtikelCategorie[0].Categorie);
+                    Assert.IsNotNull(fiets.ArtikelCategorie[0].Categorie.Naam);
+                    Assert.AreEqual(artikelEvent.Categorieen[0], fiets.ArtikelCategorie[0].Categorie.Naam);
+                }
             }
         }
 
@@ -69,21 +71,22 @@ namespace CAN.Webwinkel.Infrastructure.Test.EventListenerTest
         {
             // No strict because we dont need it for this object
             var loggerMock = new Mock<ILogger>();
-            var dispatcher = new ArtikelEventDispatcher(null, _options, loggerMock.Object);
-
-            var eventProvider = new EventProvider();
-            dispatcher.ArtikelAanCatalogusToegevoegd(eventProvider.FietsCreatedEvent);
-            dispatcher.ArtikelAanCatalogusToegevoegd(eventProvider.HerenFietsCreatedEvent);
-
-            using (var context = new WinkelDatabaseContext(_options))
-            using (var artikelRepo = new ArtikelRepository(context))
+            using (var dispatcher = new ArtikelEventDispatcher(null, _options, loggerMock.Object))
             {
-                Assert.AreEqual(2, artikelRepo.Count());
-                dispatcher.ArtikelUitCatalogusVerwijderd(eventProvider.VerwijderFietsEvent);
+                var eventProvider = new EventProvider();
+                dispatcher.ArtikelAanCatalogusToegevoegd(eventProvider.FietsCreatedEvent);
+                dispatcher.ArtikelAanCatalogusToegevoegd(eventProvider.HerenFietsCreatedEvent);
 
-                Assert.AreEqual(1, artikelRepo.Count());
-                Assert.AreEqual(2, context.Categorieen.Count());
-                Assert.ThrowsException<InvalidOperationException>(() => artikelRepo.Find(eventProvider.VerwijderFietsEvent.Artikelnummer));
+                using (var context = new WinkelDatabaseContext(_options))
+                using (var artikelRepo = new ArtikelRepository(context))
+                {
+                    Assert.AreEqual(2, artikelRepo.Count());
+                    dispatcher.ArtikelUitCatalogusVerwijderd(eventProvider.VerwijderFietsEvent);
+
+                    Assert.AreEqual(1, artikelRepo.Count());
+                    Assert.AreEqual(2, context.Categorieen.Count());
+                    Assert.ThrowsException<InvalidOperationException>(() => artikelRepo.Find(eventProvider.VerwijderFietsEvent.Artikelnummer));
+                }
             }
         }
 
@@ -93,31 +96,33 @@ namespace CAN.Webwinkel.Infrastructure.Test.EventListenerTest
         {
             // No strict because we dont need it for this object
             var loggerMock = new Mock<ILogger>();
-            var dispatcher = new ArtikelEventDispatcher(null, _options, loggerMock.Object);
-            var eventProvider = new EventProvider();
-
-            dispatcher.ArtikelAanCatalogusToegevoegd(eventProvider.FietsCreatedEvent);
-
-            using (var context = new WinkelDatabaseContext(_options))
-            using (var artikelRepo = new ArtikelRepository(context))
+            using (var dispatcher = new ArtikelEventDispatcher(null, _options, loggerMock.Object))
             {
+                var eventProvider = new EventProvider();
 
-                Assert.AreEqual(1, artikelRepo.Count());
-                var fiets = artikelRepo.Find(eventProvider.FietsCreatedEvent.Artikelnummer);
-                Assert.AreEqual(0, fiets.Voorraad);
+                dispatcher.ArtikelAanCatalogusToegevoegd(eventProvider.FietsCreatedEvent);
 
-            }
+                using (var context = new WinkelDatabaseContext(_options))
+                using (var artikelRepo = new ArtikelRepository(context))
+                {
 
-            dispatcher.ArtikelInMagazijnGezet(eventProvider.VerhoogVoorraadEvent);
+                    Assert.AreEqual(1, artikelRepo.Count());
+                    var fiets = artikelRepo.Find(eventProvider.FietsCreatedEvent.Artikelnummer);
+                    Assert.AreEqual(0, fiets.Voorraad);
 
-            using (var context = new WinkelDatabaseContext(_options))
-            using (var artikelRepo = new ArtikelRepository(context))
-            {
+                }
 
-                Assert.AreEqual(1, artikelRepo.Count());
-                var fiets = artikelRepo.Find(eventProvider.FietsCreatedEvent.Artikelnummer);
-                Assert.AreEqual(eventProvider.VerhoogVoorraadEvent.Voorraad, fiets.Voorraad);
+                dispatcher.ArtikelInMagazijnGezet(eventProvider.VerhoogVoorraadEvent);
 
+                using (var context = new WinkelDatabaseContext(_options))
+                using (var artikelRepo = new ArtikelRepository(context))
+                {
+
+                    Assert.AreEqual(1, artikelRepo.Count());
+                    var fiets = artikelRepo.Find(eventProvider.FietsCreatedEvent.Artikelnummer);
+                    Assert.AreEqual(eventProvider.VerhoogVoorraadEvent.Voorraad, fiets.Voorraad);
+
+                }
             }
         }
 
@@ -130,32 +135,36 @@ namespace CAN.Webwinkel.Infrastructure.Test.EventListenerTest
         {
             // No strict because we dont need it for this object
             var loggerMock = new Mock<ILogger>();
-            var dispatcher = new ArtikelEventDispatcher(null, _options, loggerMock.Object);
-            var eventProvider = new EventProvider();
 
-            dispatcher.ArtikelAanCatalogusToegevoegd(eventProvider.FietsCreatedEvent);
-            dispatcher.ArtikelInMagazijnGezet(eventProvider.VerhoogVoorraadEvent);
-            using (var context = new WinkelDatabaseContext(_options))
-            using (var artikelRepo = new ArtikelRepository(context))
+            using (var dispatcher = new ArtikelEventDispatcher(null, _options, loggerMock.Object))
             {
+                var eventProvider = new EventProvider();
 
-                Assert.AreEqual(1, artikelRepo.Count());
-                var fiets = artikelRepo.Find(eventProvider.FietsCreatedEvent.Artikelnummer);
-                Assert.AreEqual(eventProvider.VerhoogVoorraadEvent.Voorraad, fiets.Voorraad);
+                dispatcher.ArtikelAanCatalogusToegevoegd(eventProvider.FietsCreatedEvent);
+                dispatcher.ArtikelInMagazijnGezet(eventProvider.VerhoogVoorraadEvent);
+                using (var context = new WinkelDatabaseContext(_options))
+                using (var artikelRepo = new ArtikelRepository(context))
+                {
 
+                    Assert.AreEqual(1, artikelRepo.Count());
+                    var fiets = artikelRepo.Find(eventProvider.FietsCreatedEvent.Artikelnummer);
+                    Assert.AreEqual(eventProvider.VerhoogVoorraadEvent.Voorraad, fiets.Voorraad);
+
+                }
+
+                dispatcher.ArtikelUitMagazijnGehaald(eventProvider.VerlaagVoorraadEvent);
+
+
+                using (var context = new WinkelDatabaseContext(_options))
+                using (var artikelRepo = new ArtikelRepository(context))
+                {
+                    Assert.AreEqual(1, artikelRepo.Count());
+                    var fiets = artikelRepo.Find(eventProvider.FietsCreatedEvent.Artikelnummer);
+                    Assert.AreEqual(eventProvider.VerlaagVoorraadEvent.Voorraad, fiets.Voorraad);
+
+                }
             }
 
-            dispatcher.ArtikelUitMagazijnGehaald(eventProvider.VerlaagVoorraadEvent);
-                   
-
-            using (var context = new WinkelDatabaseContext(_options))
-            using (var artikelRepo = new ArtikelRepository(context))
-            {
-                Assert.AreEqual(1, artikelRepo.Count());
-                var fiets = artikelRepo.Find(eventProvider.FietsCreatedEvent.Artikelnummer);
-                Assert.AreEqual(eventProvider.VerlaagVoorraadEvent.Voorraad, fiets.Voorraad);
-
-            }
         }
 
     }
