98604338a72b63114c050932b9da62fcfd767c74
Robert-Jan Kooijman <RobertJanK@CURSISTZ07>
2017-01-16 13:39:35 +0100

    Winkelmandje animatie toegevoegd.

--- CAN.Webwinkel/src/CAN.Webwinkel/wwwroot/js/site.js
+++ CAN.Webwinkel/src/CAN.Webwinkel/wwwroot/js/site.js
@@ -2,5 +2,11 @@
 "use strict";
 
 function addToCart(artikel) {
-    console.log(artikel);
+    document.getElementById('addToCart').className = 'glyphicon glyphicon-shopping-cart btn btn-info animation';
+
+    $('#addToCart').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',   
+    function(e) {
+        // code to execute after transition ends
+        document.getElementById('addToCart').className = 'glyphicon glyphicon-shopping-cart btn btn-info';
+    });
 }
