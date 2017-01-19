a9573a081b7769564020797c933dc4fb7aa864d1
Rob Gerritsen <RobG@CURSISTZ03>
2017-01-19 10:02:00 +0100

    common v13

---
+++ CAN.BackOffice/CAN.BackOffice.Infrastructure.Test/RepositoriesTest/BestellingRepositoryTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CAN.BackOffice.Infrastructure.Test.RepositoriesTest
+{
+    [TestClass]
+    public class BestellingRepositoryTest
+    {
+
+        [TestMethod]
+        public void AddKlant()
+        {
+
+        }
+
+    }
+}
