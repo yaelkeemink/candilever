4704893e7f9536ae103d08e105cbcdaa5248af2c
Rob Gerritsen <RobG@CURSISTZ03>
2017-01-23 11:03:39 +0100

    Removed old project Added new project Added api for login en registration Some more things

---
+++ CAN.AuthenticatieService/src/CAN.Candeliver.BackOfficeAuthenticatie/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CAN.Candeliver.BackOfficeAuthenticatie.Models;
+
+namespace CAN.Candeliver.BackOfficeAuthenticatie.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="options"></param>
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="builder"></param>
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
