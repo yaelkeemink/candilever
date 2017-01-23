623a73fe0c173f26b684ca16d8c382192c864d77
Vincent van de Meent <VincentM@CURSISTZ04>
2017-01-23 15:49:57 +0100

    Ombouwen endpoint naar listener, service verplaats naar infrastructure. DTO eruit gehaald

---
+++ CAN.Bestellingbeheer/CAN.Bestellingbeheer/src/CAN.Bestellingbeheer.Infrastructure/Interfaces/IBestellingService.cs
@@ -0,0 +1,13 @@
+﻿using CAN.Bestellingbeheer.Domain.Entities;
+using System;
+
+namespace CAN.Bestellingbeheer.Infrastructure.Interfaces
+{
+    public interface IBestellingService
+        :IDisposable
+    {
+        int UpdateBestelling(Bestelling bestelling);
+        int StatusNaarOpgehaald(long id);
+        void CreateBestelling(Bestelling bestelling);
+    }
+}
--- CAN.Bestellingbeheer/CAN.Bestellingbeheer/src/CAN.Bestellingbeheer.Infrastructure/Interfaces/IRepository.cs
+++ CAN.Bestellingbeheer/CAN.Bestellingbeheer/src/CAN.Bestellingbeheer.Infrastructure/Interfaces/IRepository.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq.Expressions;
 
-namespace CAN.Bestellingbeheer.Domain.Interfaces
+namespace CAN.Bestellingbeheer.Infrastructure.Interfaces
 {
     public interface IRepository<TEntity, TKey>
         : IDisposable
