b041f5bf8e335980a45d9327260b8a06d8d0f81a
Michiel Blaauw <michielB@CURSISTZ08>
2017-01-25 11:21:06 +0100

    Added Domain.Test project

---
+++ CAN.Webwinkel/test/CAN.Webwinkel.Domain.Test/ServiceTests/ArtikelServiceTests.cs
@@ -0,0 +1,158 @@
+﻿
+
+using CAN.Webwinkel.Domain.Services;
+using CAN.Webwinkel.Infrastructure.DAL;
+using CAN.Webwinkel.Infrastructure.DAL.Repositories;
+using CAN.Webwinkel.Infrastructure.Test.Provider;
+using CAN.Webwinkel.Infrastructure.Test.RepositoriesTest;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace CAN.Webwinkel.Infrastructure.Test.ServiceTests
+{
+    [TestClass]
+    public class ArtikelServiceTests
+    {
+        private DbContextOptions<WinkelDatabaseContext> _options;
+        private static ArtikelService service;
+        private WinkelDatabaseContext _context;
+        private static ArtikelRepository _repo;
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestInitialize]
+        public void Init()
+        {
+            _options = TestDatabaseProvider.CreateInMemoryDatabaseOptions();
+
+            _context = new WinkelDatabaseContext(_options);
+            _repo = new ArtikelRepository(_context);
+            ILogger<ArtikelService> logger = null;
+            service = new ArtikelService(logger, _repo);
+        }
+
+        [TestMethod]
+        public static void ArtikelenBijCategorieTest()
+        {
+            //Arrange
+            var demo = new DemoEntities();
+            var herenFiets = demo.HerenFiets;
+
+
+            _repo.Insert(demo.HerenFiets);
+            _repo.Insert(demo.DamesFiets);
+            _repo.Insert(demo.Fiets);
+
+
+            //Act
+            var artikelen = service.ArtikelenBijCategorie("Heren fiets").ToArray();
+
+            //Assert
+            var herenfiets = artikelen[0];
+            var fiets = artikelen[1];
+
+            Assert.AreEqual(demo.HerenFiets.Id, herenfiets.Id);
+            Assert.AreEqual(demo.Fiets.Id, fiets.Id);
+
+        }
+
+        [TestMethod]
+        public static void AlleArtikelenTest()
+        {
+            //Arrange
+            var demo = new DemoEntities();
+            var herenFiets = demo.HerenFiets;
+
+            _repo.Insert(demo.HerenFiets);
+            _repo.Insert(demo.DamesFiets);
+            _repo.Insert(demo.Fiets);
+
+
+            //Act
+            var alleArtikelen = service.AlleArtikelen();
+
+            //Assert
+            var artikelenLength = alleArtikelen.Count();
+
+            Assert.AreEqual(3, artikelenLength);
+
+        }
+
+        [TestMethod]
+        public static void AlleArtikelenPerPaginaOnevenAantalTest()
+        {
+            //Arrange
+            var demo = new DemoEntities();
+            var customHerenFiets = demo.HerenFiets;
+            customHerenFiets.Id = 4;
+
+            var customDamesFiets = demo.DamesFiets;
+            customDamesFiets.Id = 5;
+
+
+            //Dubble Inserts Only for testing.
+            _repo.Insert(demo.HerenFiets);
+            _repo.Insert(demo.DamesFiets);
+            _repo.Insert(demo.Fiets);
+
+            _repo.Insert(customHerenFiets);
+            _repo.Insert(customDamesFiets);
+
+
+            //Act
+            var paginaNummer = 2;
+            var aantalArtikelen = 3;
+            var alleArtikelenOpPagina = service.AlleArtikelenPerPagina(paginaNummer, aantalArtikelen).ToArray();
+
+            //Assert
+            var eersteArtikelOpPagina = alleArtikelenOpPagina[0];
+            var tweedeArtikelOpPagina = alleArtikelenOpPagina[1];
+
+            var artikelenLength = alleArtikelenOpPagina.Count();
+
+            Assert.AreEqual(customHerenFiets.Id, eersteArtikelOpPagina.Id);
+            Assert.AreEqual(customDamesFiets.Id, tweedeArtikelOpPagina.Id);
+            Assert.AreEqual(2, artikelenLength); // Er zijn 5 inserts gedaan er er worden 3 artikelen overgeslagen.
+
+        }
+
+        [TestMethod]
+        public static void AantalPaginasTest()
+        {
+            //Arrange
+            var demo = new DemoEntities();
+            var customHerenFiets = demo.HerenFiets;
+            customHerenFiets.Id = 4;
+
+            var customDamesFiets = demo.DamesFiets;
+            customDamesFiets.Id = 5;
+
+            //Dubble Inserts Only for testing.
+            _repo.Insert(demo.HerenFiets);
+            _repo.Insert(demo.DamesFiets);
+            _repo.Insert(demo.Fiets);
+
+            _repo.Insert(customHerenFiets);
+            _repo.Insert(customDamesFiets);
+
+
+            //Act
+            var aantalArtikelenPerPagina = 4;
+            var aantalPaginas = service.AantalPaginas(aantalArtikelenPerPagina);
+
+            //Assert
+            var expectedAantalPaginas = 2;
+
+            Assert.AreEqual(expectedAantalPaginas, aantalPaginas);
+        }
+
+        [ClassCleanup]
+        public void Dispose()
+        {
+            _context.Dispose();
+            _repo.Dispose();
+        }
+    }
+}
---
+++ CAN.Webwinkel/test/CAN.Webwinkel.Domain.Test/ServiceTests/DemoEntities.cs
@@ -0,0 +1,161 @@
+﻿using CAN.Webwinkel.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CAN.Webwinkel.Infrastructure.Test.RepositoriesTest
+{
+    public class DemoEntities
+    {
+
+        public Artikel Fiets
+        {
+            get
+            {
+                var ar = new Artikel()
+                {
+                    Id = 1,
+                    Artikelnummer = 1,
+                    Beschrijving = "Hele mooie fiets",
+                    AfbeeldingUrl = @"http://www.scribblelive.com/wp-content/uploads/2014/06/IMG_6424-1300x866.jpg",
+                    Leverancier = "Kantilever",
+                    LeverancierCode = "KTL",
+                    LeverbaarTot = new DateTime(2017, 8, 1),
+                    LeverbaarVanaf = new DateTime(2017, 1, 1),
+                    Naam = "Fiets",
+                    Prijs = 159.67M,
+                    ArtikelCategorie = new List<ArtikelCategorie>()
+                };
+
+                ArtikelCategorie arCat = new ArtikelCategorie() {
+                    Artikel = ar,
+                    Categorie = new Categorie() {
+                        Naam = "Heren fiets"
+                    }
+                };
+                ar.ArtikelCategorie.Add(arCat);
+
+                return ar;
+            }
+        }
+
+        public Artikel HerenFiets
+        {
+            get
+            {
+                var ar = new Artikel()
+                {
+                    Id = 2,
+                    Artikelnummer = 2,
+                    Beschrijving = "Hele mooie heren fiets",
+                    AfbeeldingUrl = @"http://www.scribblelive.com/wp-content/uploads/2014/06/IMG_6424-1300x866.jpg",
+                    Leverancier = "Kantilever",
+                    LeverancierCode = "KTL",
+                    LeverbaarTot = new DateTime(2017, 8, 1),
+                    LeverbaarVanaf = new DateTime(2017, 1, 1),
+                    Naam = "Heren fiets",
+                    Prijs = 259.67M,
+                    ArtikelCategorie = new List<ArtikelCategorie>()
+                };
+
+                ArtikelCategorie arCat = new ArtikelCategorie()
+                {
+                    Artikel = ar,
+                    Categorie = new Categorie()
+                    {
+                        Naam = "Heren fiets"
+                    }
+                };
+                ar.ArtikelCategorie.Add(arCat);
+
+                return ar;
+            }
+        }
+
+        public Artikel DamesFiets
+        {
+            get
+            {
+                var ar = new Artikel()
+                {
+                    Id = 3,
+                    Artikelnummer = 3,
+                    Beschrijving = "Hele mooie dames fiets",
+                    AfbeeldingUrl = @"http://www.scribblelive.com/wp-content/uploads/2014/06/IMG_6424-1300x866.jpg",
+                    Leverancier = "Kantilever",
+                    LeverancierCode = "KTL",
+                    LeverbaarTot = new DateTime(2017, 8, 1),
+                    LeverbaarVanaf = new DateTime(2017, 1, 1),
+                    Naam = "dames fiets",
+                    Prijs = 59.67M,
+                    ArtikelCategorie = new List<ArtikelCategorie>() 
+                };
+
+                ArtikelCategorie arCat = new ArtikelCategorie()
+                {
+                    Artikel = ar,
+                    Categorie = new Categorie()
+                    {
+                        Naam = "Dames fiets"
+                    }
+                };
+                ar.ArtikelCategorie.Add(arCat);
+                return ar;
+            }
+        }
+
+        public Categorie Categorie()
+        {
+            Categorie categorie = new Categorie
+            {
+                Id = 1,
+                Naam = "Fiets Frames"
+            };
+
+            ArtikelCategorie artikelCategorie = new ArtikelCategorie
+            {
+                ArtikelId = 1,
+                Artikel = new Artikel(),
+                CategoryId = 1,
+                Categorie = categorie
+            };
+
+            categorie.ArtikelCategorie.Add(artikelCategorie);
+            return categorie;
+        }
+
+        public Categorie Categorie_2()
+        {
+            Categorie categorie_2 = new Categorie
+            {
+                Id = 2,
+                Naam = "Fiets Onderdelen"
+            };
+
+            ArtikelCategorie artikelCategorie_2 = new ArtikelCategorie
+            {
+                ArtikelId = 2,
+                Artikel = new Artikel(),
+                CategoryId = 2,
+                Categorie = categorie_2
+            };
+
+            categorie_2.ArtikelCategorie.Add(artikelCategorie_2);
+            
+
+            ArtikelCategorie artikelCategorie_3 = new ArtikelCategorie
+            {
+                ArtikelId = 2,
+                Artikel = new Artikel(),
+                CategoryId = 2,
+                Categorie = categorie_2
+            };
+
+            categorie_2.ArtikelCategorie.Add(artikelCategorie_3);
+
+
+            return categorie_2;
+        }
+    }
+}
